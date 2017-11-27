b9874e102b0d36a6c0e2b55449aded3ad510ae47
AndrewNeudegg <andrew.neudegg@gmail.com>
2017-11-27 21:10:56 +0000

    Updated Structure to Enable Pypi support.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='venv_easy',
+      version='0.1',
+      description='Easily automating virtual environment creation and implementation from within your Python3 Application.',
+      url='https://github.com/AndrewNeudegg/venv_easy',
+      author='Andrew Neudegg',
+      author_email='omitted',
+      license='MIT',
+      packages=['venv_easy'],
+      zip_safe=False)
